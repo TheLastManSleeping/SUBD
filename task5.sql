7563256a03e4adc3af8e2c4c457e8fc13753de83
Артер <62806118+TheLastManSleeping@users.noreply.github.com>
2020-11-15 00:57:03 +0300

    Update task5.sql

--- task5.sql
+++ task5.sql
@@ -1,4 +1,5 @@
 ﻿use MyData;
 
 select count(case when name2 like 'Халипо_' then name2 end) as s1, 
-count(case when name2 like 'Халип__' then name2 end) from surname as s2;    
+count(case when name2 like 'Халип__' then name2 end) as s2
+from surname;    
