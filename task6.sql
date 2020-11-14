914a6e912310d57094a8fa9b2a500bdbd1efd84d
Артер <62806118+TheLastManSleeping@users.noreply.github.com>
2020-11-15 00:54:23 +0300

    Update task6.sql

--- task6.sql
+++ task6.sql
@@ -1,4 +1,4 @@
 ﻿use MyData;
 
-select count(case when name1 like 'Арту_' then name1 end), 
-count(case when name1 like 'Арт__' then name1 end) from name;    
+select count(case when name1 like 'Арту_' then name1 end) as n1, 
+count(case when name1 like 'Арт__' then name1 end) from name as n2;    
